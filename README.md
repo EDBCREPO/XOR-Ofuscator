9f61f63d957ed1bcb58d21b2e54cf1c100b6a4e8
EDBC <emaill>
2024-07-10 20:01:07 -0400

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+# XOR-Ofuscator
+The most simplest XOR Ofuscator
+
+# Usage
+```bash
+XOR ?out=/PATH/TO/FILE/A ?in=/PATH/TO/FILE/B ?sec=SECRET_KEY
+```
+
+# Build
+```bash
+# Clone the project
+💻: git clone https://github.com/EDBCREPO/XOR-Ofuscator
+
+# Compile the Porject
+💻: g++ -o XOR main.cpp
+
+```
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <nodepp/nodepp.h>
+#include <nodepp/fs.h>
+
+using namespace nodepp;
+
+void onMain() {
+
+    if( process::env::get("out").empty() ){ process::error("no output file found"); }
+    if( process::env::get("sec").empty() ){ process::error("no secret key found"); }
+    if( process::env::get("in").empty()  ){ process::error("no input file found"); }
+
+    auto fout = fs::writable( process::env::get("out") );
+    auto file = fs::readable( process::env::get("in") );
+    auto sec  = process::env::get("sec");
+
+    file.onClose([](){ console::log("done"); });
+
+    file.onData([=]( string_t data ){
+        ulong pos = 0; forEach( x, data ){
+            x = x ^ sec[pos]; pos++;
+        }   fout.write( data );
+    });
+
+    stream::pipe( file );
+
+}
