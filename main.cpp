4bfc565c8f4f9623feccaf390b4e20e31435036f
EDBC <EDBC>
2024-07-12 12:24:12 -0400

    update

--- main.cpp
+++ main.cpp
@@ -3,11 +3,7 @@
 
 using namespace nodepp;
 
-void onMain() {
-
-    if( process::env::get("out").empty() ){ process::error("no output file found"); }
-    if( process::env::get("sec").empty() ){ process::error("no secret key found"); }
-    if( process::env::get("in").empty()  ){ process::error("no input file found"); }
+void normal() {
 
     auto fout = fs::writable( process::env::get("out") );
     auto file = fs::readable( process::env::get("in") );
@@ -23,5 +19,37 @@ void onMain() {
     });
 
     stream::pipe( file );
+    
+}
+
+void abnormal() {
+
+    auto file = fs::readable( process::env::get("in") );
+    auto sec  = process::env::get("sec");
+    auto data = stream::await( file );
+
+    ulong pos = 0; forEach( x, data ){
+        x = x^sec[pos]; pos++;
+    }
+
+    auto fout = fs::writable( process::env::get("out") );
+    fout.write( data );
+
+}
+
+void onMain() {
+
+    if( process::env::get("sec").empty() ){ process::error("no secret key found"); }
+
+    if( process::env::get("file").empty() ){
+    if( process::env::get("out").empty() ){ process::error("no output file found"); }
+    if( process::env::get("in").empty()  ){ process::error("no input file found"); }
+    } else { 
+        process::env::set( "out", process::env::get("file") );
+        process::env::set( "in" , process::env::get("file") );
+    }
+
+    if( process::env::get("out") == process::env::get("in") )
+      { abnormal(); } else { normal(); }
 
 }
